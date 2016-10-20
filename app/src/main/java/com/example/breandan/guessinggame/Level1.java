baf1e3a07e6dd8fe3b7af3f748a550c99b9a3511
Breandan Heiliger <breandanh@gmail.com>
2016-10-20 13:32:09 -0700

    Progress

--- app/src/main/java/com/example/breandan/guessinggame/Level1.java
+++ app/src/main/java/com/example/breandan/guessinggame/Level1.java
@@ -9,5 +9,50 @@ public class Level1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level1);
+
+        //Make a button from each of the 3 buttons in our layout
+        Button button1 = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+
+        //make each of them listen for clicks
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+
+
     }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId())
+        {
+            case R.id.button1:  //when the first button is pressed
+
+                //Check if pressed button matches random button.
+
+                //If good guess, then see how many attempts and give correct amount of points.
+                //Also, play correct sound and then start next leve.
+
+                //If not, play sound for incorrect answer,reduce points possible and increment counter.
+
+                //play sample 1
+                soundPool.play(sample1,1,1,0,0,1);
+
+                break;
+            case R.id.button2:  //when the first button is pressed
+                //play sample 1
+                soundPool.play(sample2,1,1,0,0,1);
+
+
+
+                break;
+            case R.id.button3:  //when the first button is pressed
+                //play sample 1
+                soundPool.play(sample3,1,1,0,0,1);
+                break;
+
+        }
+
 }
--- app/src/main/java/com/example/breandan/guessinggame/MainActivity.java
+++ app/src/main/java/com/example/breandan/guessinggame/MainActivity.java
@@ -1,18 +1,135 @@
 package com.example.breandan.guessinggame;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
+import java.io.IOException;
+import java.util.logging.Level;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Used to play start sound.
+    private SoundPool soundPool;
+
+    int playsound = -1;
+
+    //Used to load persistent high score.
+    SharedPreferences prefs;
+
+    //The play button.
+    public Button playButton;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String theHighScore = "00";
+
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+
+        //Try to get start sound.
+        try {
+            AssetManager assetsManager = getAssets();
+            AssetFileDescriptor descriptor;
+
+            //Create play sound in memory.
+            descriptor = assetsManager.openFd("start.wav");
+            playsound = soundPool.load(descriptor, 0);
+
+        } catch (IOException e) {
+            Context context = getApplicationContext();
+            CharSequence text = "File not found!";
+            int duration = Toast.LENGTH_LONG;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+        }
 
 
+
+        //Set button
+        playButton = (Button) findViewById(R.id.play);
+        playButton.setOnClickListener(this);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //Play start sound.
+        soundPool.play(playsound, 1, 1, 0, 0, 1);
+
+        //Create new intent for level1 and start level.
+        Intent m;
+        m = new Intent(this, Level1.class);
+        startActivity(m);
+
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.breandan.guessinggame/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.breandan.guessinggame/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
